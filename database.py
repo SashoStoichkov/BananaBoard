627c9800a1617763377c83d2f0f07515e5af94f1
Alexander Stoichkov <sashostoichkov@gmail.com>
2020-06-05 17:17:33 +0300

    basic setup

---
+++ database.py
@@ -0,0 +1,42 @@
+import sqlite3
+
+DB_NAME = 'database.db'
+
+conn = sqlite3.connect(DB_NAME)
+
+# conn.cursor().execute('''
+# CREATE TABLE IF NOT EXISTS products
+#     (
+#         id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         title TEXT,
+#         content TEXT,
+#         price REAL,
+#         published TIMESTAMP,
+#         is_active INTEGER NOT NULL,
+#         owner_id INTEGER,
+#         publisher_id INTEGER,
+#         FOREIGN KEY(owner_id) REFERENCES users(id)
+#         FOREIGN KEY(publisher_id) REFERENCES users(id)
+#     )
+# ''')
+# conn.cursor().execute('''
+# CREATE TABLE IF NOT EXISTS users
+#     (
+#         id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         name TEXT UNIQUE NOT NULL,
+#         password TEXT NOT NULL,
+#         email TEXT UNIQUE NOT NULL,
+#         address TEXT NOT NULL,
+#         phone TEXT NOT NULL
+#     )
+# ''')
+conn.commit()
+
+
+class DB:
+    def __enter__(self):
+        self.conn = sqlite3.connect(DB_NAME)
+        return self.conn.cursor()
+
+    def __exit__(self, type, value, traceback):
+        self.conn.commit()
