0996fd67f39a701a5ba2774c53b6f7149fa88d4e
Alexander Stoichkov <sashostoichkov@gmail.com>
2020-06-08 21:23:16 +0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # SUBD-Project
+
+## Таблици
+* users
+* tasks
+* task_status
+* task_count_per_user
+* task_type
+
+## Страници
+* landing page
+* login/register
+* board
+* card_edit/delete
--- database.py
+++ database.py
@@ -4,17 +4,17 @@ DB_NAME = 'database.db'
 
 conn = sqlite3.connect(DB_NAME)
 
-# conn.cursor().execute('''
-# CREATE TABLE IF NOT EXISTS users
-#     (
-#         id INTEGER PRIMARY KEY AUTOINCREMENT,
-#         name TEXT UNIQUE NOT NULL,
-#         password TEXT NOT NULL,
-#         email TEXT UNIQUE NOT NULL,
-#         address TEXT NOT NULL,
-#         phone TEXT NOT NULL
-#     )
-# ''')
+conn.cursor().execute(
+    '''
+        CREATE TABLE IF NOT EXISTS users
+            (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT UNIQUE NOT NULL,
+                password TEXT NOT NULL,
+                email TEXT UNIQUE NOT NULL
+            )
+    '''
+)
 conn.commit()
 
 
