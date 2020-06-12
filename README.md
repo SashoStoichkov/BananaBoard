b1dad85291489bc01552e5d2d6ce67a54ba73792
Iliana Genova <iliana.genova.02@gmail.com>
2020-06-12 22:58:52 +0300

    CRUD for tasks

--- README.md
+++ README.md
@@ -14,12 +14,12 @@
 * card_edit/delete
 
 ## Функционалност
-* CRUD operations (4)
-* select all tasks of *task_status*
-* select all tasks of *task_type*
-* select all tasks by user id
-* count all tasks by user id
-* select title/content/status/type by id of task
+* CRUD operations (4) c+ r+ u+ d+
+* select all tasks of *task_status* +
+* select all tasks of *task_type* +
+* select all tasks by user id +
+* count all tasks by user id +
+* update title/content/status/type by id of task +
 
 ## TBD
 * task importancy?
---
+++ task.py
@@ -0,0 +1,246 @@
+from database import DB
+
+class Task:
+    def __init__(self, title, content, task_status_id, task_type_id):
+        this.id = None
+        this.title = title
+        this.content = content
+        this.task_status_id = task_status_id
+        this.task_type_id = task_type_id
+
+    def create(self, user_id):
+        with DB() as db:
+            values = (None, self.title, self.content, self.task_status_id, self.task_type_id)
+            db.execute(
+                '''
+                    INSERT INTO tasks
+                    VALUES (?, ?, ?, ?, ?)
+                ''', values
+            )
+            id = db.execute(
+                '''
+                    SELECT id
+                    FROM tasks
+                    WHERE tasks.title = ?
+                ''', (self.title,)
+            ).fetchone()
+            db.execute(
+                '''
+                    INSERT INTO user_tasks
+                    VALUES (?, ?)
+                ''', (user_id, id)
+            )
+            return self
+
+    @staticmethod
+    def display_all(user_id):
+        with DB() as db:
+            return db.execute(
+                '''
+                    SELECT tasks.title, tasks.content, task_types.title
+                    FROM tasks
+                    INNER JOIN task_types
+                    ON tasks.task_type_id = task_type.id
+                    INNER JOIN user_tasks
+                    ON tasks.id = user_tasks.task_id
+                    INNER JOIN users
+                    ON user_tasks.user_id = users.id
+                    WHERE users.id = ?
+                ''', (user_id,)
+            ).fetchall()
+
+    @staticmethod
+    def count_all(user_id):
+        with DB() as db:
+            return db.execute(
+                '''
+                    SELECT COUNT(user_tasks.task_id)
+                    FROM user_tasks
+                    WHERE users.id = ?
+                ''', (user_id,)
+            ).fetchone()
+
+    @staticmethod
+    def display_by_status(user_id, status_id):
+        with DB() as db:
+            return db.execute(
+                '''
+                    SELECT tasks.title, tasks.content, task_types.title
+                    FROM tasks
+                    INNER JOIN task_types
+                    ON tasks.task_type_id = task_type.id
+                    INNER JOIN user_tasks
+                    ON tasks.id = user_tasks.task_id
+                    INNER JOIN users
+                    ON user_tasks.user_id = users.id
+                    WHERE users.id = ? AND tasks.task_status_id = ?
+                ''', (user_id, status_id)
+            ).fetchall()
+
+
+    @staticmethod
+    def display_by_type(user_id, task_type):
+        with DB() as db:
+            return db.execute(
+                '''
+                    SELECT tasks.title, tasks.content, task_types.title
+                    FROM tasks
+                    INNER JOIN task_types
+                    ON tasks.task_type_id = task_type.id
+                    INNER JOIN user_tasks
+                    ON tasks.id = user_tasks.task_id
+                    INNER JOIN users
+                    ON user_tasks.user_id = users.id
+                    WHERE users.id = ? AND task_types.title = ?
+                ''', (user_id, task_type)
+            ).fetchall()
+
+
+    @staticmethod
+    def select_by_title(user_id, title):
+        with DB() as db:
+            row = db.execute(
+                '''
+                    SELECT tasks.title, tasks.content, tasks.task_type_id, tasks.task_status_id
+                    FROM tasks
+                    INNER JOIN user_tasks
+                    ON tasks.id = user_tasks.task_id
+                    WHERE user_tasks.user_id = ? AND tasks.title = ?
+                ''', (user_id, title)
+            ).fetchone()
+
+            return Task(*row)
+
+
+    @staticmethod
+    def select_by_title(user_id, title):
+        with DB() as db:
+            return db.execute(
+                '''
+                    SELECT tasks.title, tasks.content, tasks.task_status_id, tasks.task_type_id
+                    FROM tasks
+                    INNER JOIN user_tasks
+                    ON tasks.id = user_tasks.task_id
+                    WHERE user_tasks.user_id = ? AND tasks.title = ?
+                ''', (user_id, title)
+            ).fetchone()
+
+
+    @staticmethod
+    def edit_content(task_id, content):
+        if not content:
+            return None
+
+        with DB() as db:
+            row = db.execute(
+                '''
+                    SELECT *
+                    FROM tasks
+                    WHERE id = ?
+                ''', (task_id,)
+            ).fetchone()
+
+            if row:
+                db.execute(
+                    '''
+                        UPDATE tasks
+                        SET content = ?
+                        WHERE id = ?
+                    ''', (content, task_id)
+                )
+
+            else:
+                return False
+
+
+    @staticmethod
+    def edit_title(task_id, title):
+        if not title:
+            return None
+
+        with DB() as db:
+            row = db.execute(
+                '''
+                    SELECT *
+                    FROM tasks
+                    WHERE id = ?
+                ''', (task_id,)
+            ).fetchone()
+
+            if row:
+                db.execute(
+                    '''
+                        UPDATE tasks
+                        SET title = ?
+                        WHERE id = ?
+                    ''', (title, task_id)
+                )
+
+            else:
+                return False
+
+
+    @staticmethod
+    def edit_type(task_id, type):
+        if not type:
+            return None
+
+        with DB() as db:
+            row = db.execute(
+                '''
+                    SELECT *
+                    FROM tasks
+                    WHERE id = ?
+                ''', (task_id,)
+            ).fetchone()
+
+            if row:
+                db.execute(
+                    '''
+                        UPDATE tasks
+                        SET type = ?
+                        WHERE id = ?
+                    ''', (type, task_id)
+                )
+
+            else:
+                return False
+
+
+    @staticmethod
+    def edit_status(task_id, status):
+        if not status:
+            return None
+
+        with DB() as db:
+            row = db.execute(
+                '''
+                    SELECT *
+                    FROM tasks
+                    WHERE id = ?
+                ''', (task_id,)
+            ).fetchone()
+
+            if row:
+                db.execute(
+                    '''
+                        UPDATE tasks
+                        SET content = ?
+                        WHERE id = ?
+                    ''', (status, task_id)
+                )
+
+            else:
+                return False
+
+
+    def delete(self):
+        with DB() as db:
+            db.execute(
+                '''
+                    DELETE FROM tasks
+                    WHERE title = ?
+                ''', (self.title,)
+            )
+
+            return self
